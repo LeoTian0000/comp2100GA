edd31eef4d4fd1dfe031a8210c1b925e7bda4808
ZHANG ZHONGTIAN <u6936049@anu.edu.au>
2019-09-29 14:43:49 +1000

    Initial commit

---
+++ app/src/main/java/com/example/u6936049/comp2100ga/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.u6936049.comp2100ga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
